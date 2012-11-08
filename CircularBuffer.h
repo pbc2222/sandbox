04026321ac800e1c99fb27ed862d5db4246d0689
Peter B. Christiansen <pbc@t-doc.com>
2012-11-08 03:37:13 -0500

    Initial version

---
+++ CircularBuffer.h
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+/*
+  Ring buffer 
+ */
+class CircularBuffer
+{
+public:
+  CircularBuffer(size_t capacity);
+  ~CircularBuffer();
+
+  // Return number of bytes in the data array
+  // Retrieve the internal circular data buffer as a liniar array
+  // NOTE: overrun may occur if memory allocated to data is too small
+  size_t retrieveDataArray(char *data); // Pointer to memory where buffer data is put
+
+  // Return number of bytes used 
+  size_t bytesUsed(void);
+
+  // Return number of free bytes 
+  size_t bytesFree(void);
+  //
+  // Return number of bytes actually written.
+  size_t write(
+    const char *data, // Pointer to data to be written
+    size_t bytes);    // Number of bytes to be written
+
+  // Return number of bytes read.
+  // NOTE: overrun may occur if memory allocated to data is too small
+  size_t read(
+    char *data,    // Pointer to memory where read data is put
+    size_t bytes); // Number of bytes to read
+
+private:
+  size_t beg_index_;
+  size_t end_index_;
+  size_t size_;      // Number of bytes contained in buffer
+  size_t capacity_;  // Number of bytes allocated for buffer
+  char *data_;
+};
